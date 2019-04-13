2bf5f7407048f38a8cac07507a7745cbdf0445fc
mayuri <mayuripatil2392@gmail.com>
2019-04-13 16:25:51 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';  
+/* 
+function App(){
+  return <div><Math  a={1} b={2} /></div>
+} */
+
+/* function App(){
+return <div>
+  <Body text='123'>
+  <Math  a={1} b={2} />
+  <br />
+  <a href= "www.google.com">Go to google.com</a>
+  <List items = {items} />
+  </Body>
+</div>
+} */
+
+const App = () => {
+  return <Email />
+}
+
+
+const Email = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const changeValue = (email) => {
+    setEmail(email.target.value);
+    
+  }
+  const changePValue = (password) => {
+    setPassword(password.target.value);
+  }
+  return <div><input 
+    type = 'email' 
+    value = {email}
+    onChange = { changeValue } 
+  />
+
+  <input 
+    type = 'password' 
+    value = {password}
+    onChange = { changePValue } 
+  />
+
+  <BigText  email ={email}/>
+  </div>
+}
+
+const BigText = ({email}) => {
+  return <h1>{email}</h1>
+}
+/* function App(){
+  const items = ['Asrto', 'Lab', 'JLT'];
+  return <div>
+    <Button />
+    <Counter />
+  </div>
+}
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0)
+  const incrementValue = () => {
+    if(counter < 5){
+    setCounter(counter + 1) 
+    } 
+  }
+  const decrementValue = () => {
+    if(counter > 0){
+    setCounter(counter - 1)
+    }
+  }
+  return <div>
+    { counter }
+    <button onClick = {incrementValue}>Increment</button>
+    <button onClick = {decrementValue}>Decrement</button>
+  </div>
+}
+
+const Button = () => {
+  const [color, setColor] = useState('red');
+  const changeColor = () => {
+    if(color == 'red'){
+      setColor('green');
+    }else {
+      setColor('red');
+    }
+    
+  }
+  return <div>
+    <button onClick = { changeColor }
+    style={{color: color}}>
+    Change Color!</button>
+    </div>
+}
+
+const List = ({items}) => {
+  return <div>
+     {items.map((item) => {
+        return <h1 key={item}>{item}</h1>
+    })}
+  </div>
+}
+
+const Body = (props) => {
+  return <div>{props.children}</div>
+}
+function Math(props){
+  return <PlusOperation x={props.a} y={props.b} />
+}
+
+function PlusOperation(props){
+  return <div>{props.x + props.y}</div>
+}
+ */
+export default App;
